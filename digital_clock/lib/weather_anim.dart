95f4e454313587068296300579dd2bf17198e7b5
mMiciahmae <miciahmae@gmail.com>
2020-01-20 21:42:34 -0700

    adding some comments and removing unnecessary files

--- digital_clock/lib/weather_anim.dart
+++ digital_clock/lib/weather_anim.dart
@@ -3,11 +3,19 @@ import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+///This Stateless widget handles the weather animations through Flare Actor.
+
+/// weather_anim uses logic to check Weather inputs by users
+/// and then calls (inputweather).flr files through [FlareActor] based on the weather input, if it's valid.
+
+// creates a weather class to pass each weather input in to
 class WeatherAnim extends StatelessWidget {
   const WeatherAnim({
     Key key,
   }) : super(key: key);
 
+  // checks if user input is valid weather input.
+  // we were pretty sure this was true but... just in case.
   bool isValidWeather(String weather) {
     if (weather == "cloudy" ||
         weather == "sunny" ||
@@ -24,14 +32,22 @@ class WeatherAnim extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /// gets current weather string from current clock model so isValidWeather function can read it in
     final String weather = InheritedClockModel.of(context).model.weatherString;
 
+    //  i created all of these animations in flare, exported them as binary files,
+    //  and stored each in the third_party folder
     if (isValidWeather(weather) == true) {
       return new FlareActor(
         "third_party/$weather.flr",
         animation: "Untitled",
       );
-    } else {
+    }
+
+    // okay, honestly i was just testing for each individual weather animation as i created them
+    // and it was just too fun to trigger a "THERE's NO WEATHER HERE" message
+    // so that's still here.
+    else {
       return Container(
           color: Colors.grey,
           child: Text(
