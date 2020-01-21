83879109639e8a95777cb98f90e4541d52422e0f
BuildTools <unconfigured@null.spigotmc.org>
2020-01-20 22:29:30 -0700

    Added even more, MORE comments!

--- digital_clock/lib/weather_clock.dart
+++ digital_clock/lib/weather_clock.dart
@@ -1,4 +1,4 @@
-// Copyright 2020 Bytes of Business, LLC All rights reserved.
+// Copyright 2020 Bytes of Business, LLC. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
