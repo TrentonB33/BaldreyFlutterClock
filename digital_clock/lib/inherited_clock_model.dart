88d36dca68ee87b3df5a5e5878a979bf87f8a600
BuildTools <unconfigured@null.spigotmc.org>
2020-01-20 21:32:04 -0700

    Added even more comments!

--- digital_clock/lib/inherited_clock_model.dart
+++ digital_clock/lib/inherited_clock_model.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clock_helper/model.dart';
 
+///This is an InheritedWidget that passes along the [ClockModel] to children widgets.
+///
+/// InheritedClockModel takes in a model that nearly all the other children
+/// widgets need data from so that it can be accessed universally.
 class InheritedClockModel extends InheritedWidget {
   const InheritedClockModel({
     Key key,
@@ -9,6 +13,9 @@ class InheritedClockModel extends InheritedWidget {
   })  : assert(child != null),
         super(key: key, child: child);
 
+  ///[ClockModel] that was provided for this contest.
+  ///
+  /// This model tracks things like time formatting settings, weather and the like.
   final ClockModel model;
 
   static InheritedClockModel of(BuildContext context) {
--- digital_clock/lib/themes.dart
+++ digital_clock/lib/themes.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// Our best attempt at a dynamic theme provider.
+///
+/// Originally intended to provide themes for each of the different weather
+/// conditions, this class ultimately just serves up our brightness themes
+/// based on the brightness reported to us.
 class WeatherThemes {
   WeatherThemes();
 
@@ -11,20 +16,28 @@ class WeatherThemes {
     }
   }
 
+  /// This is the theme that's provided with high brightness.
   final ThemeData _lightTheme = ThemeData(
     primaryColor: Color(0xff718093),
     accentColor: Colors.white,
   );
 
+  ///This is the theme that's provided with low brightness.
   final ThemeData _darkTheme = ThemeData(
     primaryColor: Colors.white,
     accentColor: Colors.blueGrey[600],
   );
 }
 
+///Class that provides a [Theme] Widget with appropriate [ThemeData].
+///
+/// As stated earlier, this class combined with the WeatherThemes was supposed
+/// to act as a theme provider for all different types of weather. But that was
+/// a little too ambitious, so we stuck with just "light" and "dark" themes.
 class WeatherTheme extends StatefulWidget {
   const WeatherTheme({this.child});
 
+  ///This is the child for the custom [Theme].
   final Widget child;
 
   @override
